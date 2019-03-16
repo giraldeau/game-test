987ae873febc92679ef33677567ea831a63b239e
Alexandre Gauthier <alexandre.gauthier12@gmail.com>
2019-03-16 14:14:26 -0400

    I just learned about arrow notation.

--- src/systems/physics.cpp
+++ src/systems/physics.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 void physicsHandler(State& state, float time) {
   for(vector<Entity>::iterator it = state.entities.begin(); it != state.entities.end(); ++it) {
-    if ((*it).hasPhysics) {
-      (*it).physics.velocity.x += (*it).physics.acceleration.x * time;
-      (*it).physics.velocity.y += (*it).physics.acceleration.y * time;
-      (*it).physics.velocity.x += (*it).physics.friction * time;
-      (*it).physics.velocity.y += (*it).physics.friction * time;
-      (*it).position.x += (*it).physics.velocity.x * time;
-      (*it).position.y += (*it).physics.velocity.y * time;
+    if (it->hasPhysics) {
+      it->physics.velocity.x += it->physics.acceleration.x * time;
+      it->physics.velocity.y += it->physics.acceleration.y * time;
+      it->physics.velocity.x += it->physics.friction * time;
+      it->physics.velocity.y += it->physics.friction * time;
+      it->position.x += it->physics.velocity.x * time;
+      it->position.y += it->physics.velocity.y * time;
     } else {
     }
   }
